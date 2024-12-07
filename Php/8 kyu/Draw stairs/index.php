51274c4120cd2058054207d57e88c94e4e975c2d
Thomas-Bushido <thomas.pereira59@outlook.fr>
2024-12-07 16:29:49 +0100

    071224

--- Php/8 kyu/Draw stairs/index.php
+++ Php/8 kyu/Draw stairs/index.php
@@ -4,3 +4,16 @@
 
 // For example n = 3 result in:
 // "I\n I\n  I"
+
+function draw_stairs($n) {
+    if (!$n) {
+      return "";
+    }
+    $stairs = "I";
+    for ($i = 1; $i < $n; $i += 1) {
+      $stairs .= "\n" . str_repeat(" ", $i) . "I";
+    }
+    return $stairs;
+  }
+  $n = 3; 
+  echo draw_stairs($n);
