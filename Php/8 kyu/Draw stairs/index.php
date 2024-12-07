60c602f010fd4cf3fe0af0b50991e49ede5c7473
Thomas-Bushido <thomas.pereira59@outlook.fr>
2024-12-07 16:28:16 +0100

    drawstairs modified

--- Php/8 kyu/Draw stairs/index.php
+++ Php/8 kyu/Draw stairs/index.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+// Draw stairs
+// Given a number n, draw stairs using the letter "I", n tall and n wide, with the tallest in the top left.
+
+// For example n = 3 result in:
+// "I\n I\n  I"
