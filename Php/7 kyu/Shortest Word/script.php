4e6de5cb1a699e4d100a990d9589aa72aca31eb1
Thomas-Bushido <thomas.pereira59@outlook.fr>
2024-12-11 11:31:22 +0100

    Shortest Word

---
+++ Php/7 kyu/Shortest Word/script.php
@@ -0,0 +1,20 @@
+<?php
+  
+function findShort(string $str): int{
+  
+  $words = explode(" ", $str);
+
+  $min_length = strlen($words[0]);
+
+for($i = 1; $i < count($words); $i++){
+   if(strlen($words[$i]) < $min_length){
+    $min_length = strlen($words[$i]);
+}
+
+}
+return $min_length;
+}
+
+$str = "bitcoin take over the world maybe who knows perhaps";
+
+findShort($str);
